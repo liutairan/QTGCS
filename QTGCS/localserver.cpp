82decf76ef1a0748765f941889c887ddeda2f229
Tairan Liu <liutairan2012@gmail.com>
2018-05-03 09:58:16 -0500

    Solve server reconnect problem

--- QTGCS/localserver.cpp
+++ QTGCS/localserver.cpp
@@ -30,7 +30,19 @@ void LocalServer::newConnection()
     tempSocket = server->nextPendingConnection();
     qDebug() << tempSocket->localAddress() << tempSocket->localPort();
     connect(tempSocket, SIGNAL(readyRead()), this, SLOT(readData()));
+    // Clear old connections
+    //    If not clear old connections, then old connection still
+    //    stays in the socketList, then it will give errors.
+    //    For example, if the connection comes from a cellphone.
+    //    If the cellphone gets locked and unlocked,
+    //    then the socket will not be able to receive the data
+    //    from the app. Reconnect from the app will cause the problem.
+    //    After clear the old connections, then append the new connection,
+    //    this problem can be solved because it is connected with a new
+    //    socket everytime.
+    socketList.clear();
     socketList.append(tempSocket);
+    //qDebug() << socketList;
 }
 
 void LocalServer::readData()
